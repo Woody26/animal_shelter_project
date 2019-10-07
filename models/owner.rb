d24758f031e8d3afe05d581f3f75d7b7ae00162e
Andrew Wood <ajwood2001@gmail.com>
2019-10-07 16:47:27 +0100

    owner.rb update function written

--- models/owner.rb
+++ models/owner.rb
@@ -25,8 +25,20 @@ class Owner
         @id = SqlRunner.run(sql, values)[0]['id'].to_i()
   end
 
-  #def update()
-  #end
+  def update()
+    sql = "UPDATE owner
+    SET
+    (
+      first_name,
+      last_name
+    ) =
+    (
+      $1, $2
+    )
+    WHERE id = $3"
+    values = [@first_name, @last_name, @owner_id]
+    SqlRunner.run( sql, values )
+  end
 
   def self.all()
     sql = "SELECT * FROM owner"
