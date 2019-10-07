b3756614018e3c4b7fb6204a16360e1eddf081d3
Andrew Wood <ajwood2001@gmail.com>
2019-10-07 10:48:07 +0100

    completed animal controller

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -10,3 +10,36 @@ get '/animals' do
   @animals = animal.all
   erb(:index)
 end
+
+get '/animals/new' do
+  @animals = animals.all
+  erb(:new)
+end
+
+post '/animals' do
+  Animal.new(params).save
+  redirect to '/animals'
+end
+
+get '/canimals/:id' do
+  @animals = Animal.find(params['id'])
+  erb(:show)
+end
+
+get '/animals/:id/edit' do
+  @customers = Customer.all
+  @animal = Animal.find(params['id'])
+  erb(:edit)
+end
+
+post '/animals/:id' do
+  animal = Animal.new(params)
+  animal.update
+  redirect to "/animals/#{params['id']}"
+end
+
+post '/animals/:id/delete' do
+  animal = Animal.find(params['id'])
+  animal.delete
+  redirect to '/animals'
+end
