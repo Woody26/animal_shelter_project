c8d01d2a654b21fadfe9032281f7cfff471d9fbb
Andrew Wood <ajwood2001@gmail.com>
2019-10-08 09:38:41 +0100

    Submit forms on both Animal Admissions and Create Owner working

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -16,30 +16,35 @@ get '/animal/new' do
   erb(:"/animal/new")
 end
 
-# post '/animal' do
-#   Animal.new(params).save
-#   redirect to '/animal'
-# end
-#
-# get '/animal/:id' do
-#   @animals = Animal.find(params['id'])
-#   erb(:show)
-# end
-#
-# get '/animal/:id/edit' do
-#   @owners = Owner.all
-#   @animals = Animal.find(params['id'])
-#   erb(:edit)
-# end
-#
-# post '/animal/:id' do
-#   animal = Animal.new(params)
-#   Animal.update
-#   redirect to "/animal/#{params['id']}"
-# end
-#
-# post '/animal/:id/delete' do
-#   animal = Animal.find(params['id'])
-#   animal.delete
-#   redirect to '/animal'
-# end
+#create
+post '/animal' do
+  Animal.new(params).save
+  redirect to '/animal'
+end
+
+# show
+get '/animal/:id' do
+  @animals = Animal.find(params['id'])
+  erb(:show)
+end
+
+# edit
+get '/animal/:id/edit' do
+  @owners = Owner.all
+  @animals = Animal.find(params['id'])
+  erb(:edit)
+end
+
+# update
+post '/animal/:id' do
+  animal = Animal.new(params)
+  Animal.update
+  redirect to "/animal/#{params['id']}"
+end
+
+# delete
+post '/animal/:id/delete' do
+  animal = Animal.find(params['id'])
+  Animal.delete
+  redirect to '/animal'
+end
--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -16,30 +16,35 @@ get '/owner/new' do
   erb(:"/owner/new")
 end
 
-# post '/owner' do
-#   Owner.new(params).save
-#   redirect to '/owner'
-# end
-#
-# get '/owner/:id' do
-#   @owner = Owner.find(params['id'])
-#   erb(:show)
-# end
-#
-# get '/owner/:id/edit' do
-#   @animals = Animals.all
-#   @owner = Owner.find(params['id'])
-#   erb(:edit)
-# end
-#
-# post '/owner/:id' do
-#   owner = Owner.new(params)
-#   owner.update
-#   redirect to "/owner/#{params['id']}"
-# end
-#
-# post '/owner/:id/delete' do
-#   owner = Owner.find(params['id'])
-#   Owner.delete
-#   redirect to '/owner'
-# end
+# Create
+post '/owner' do
+  Owner.new(params).save
+  redirect to '/owner'
+end
+
+# Show
+get '/owner/:id' do
+  @owner = Owner.find(params['id'])
+  erb(:show)
+end
+
+# Edit
+get '/owner/:id/edit' do
+  @animals = Animals.all
+  @owner = Owner.find(params['id'])
+  erb(:edit)
+end
+
+# Update
+post '/owner/:id' do
+  owner = Owner.new(params)
+  Owner.update
+  redirect to "/owner/#{params['id']}"
+end
+
+# Delete
+post '/owner/:id/delete' do
+  owner = Owner.find(params['id'])
+  Owner.delete
+  redirect to '/owner'
+end
